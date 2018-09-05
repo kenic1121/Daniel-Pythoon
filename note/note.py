5a812ba183b0418e20a2509bc0c7225417f11827
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2018-09-05 10:08:25 -0700

    notes

---
+++ note/note.py
@@ -0,0 +1,22 @@
+#float
+float_number = 9.0
+print (float_number)
+
+print (type(float_number))
+
+#string
+string = "hello tacoma"
+print (string)
+
+print (type(string))
+
+#math
+
+print (2 + 2)
+
+
+x = 2
+y = 6
+print (x*y)
+
+print ('enter a number-')

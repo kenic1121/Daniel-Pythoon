da8131e460ab165afd057799d07a097c24e46d1b
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2018-09-07 09:15:58 -0700

    things

---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,49 @@
+python = "I love Python"
+print (python)
+
+math = "I'll show you some math:"
+somemath = 5**45
+magic_number = 75
+floating_number = 5.129381
+
+print (math)
+print (somemath)
+print (floating_number)
+print (magic_number)
+
+
+print ('What is your name?')
+
+MyName = input()
+#print (MyName)
+
+print ("My name is", MyName)
+#if MyName == "boio":
+#    print ("Welcome back boio ;)")
+#else:
+#    print ("CYA ;(")
+
+print ("What would you like to add?")
+
+print ("1st Number")
+num1 = input ()
+print ("2nd Number")
+num2 = input ()
+
+
+
+print (num1)
+print (num2)
+print (int(num1) + int(num2))
+
+intA = 25
+intB = 25
+intC = 25
+print (intA == intB)
+#== means equal
+#!= means not equal
+#< = less than
+#> = greater than
+#<= = less than or equal to
+#>= = greater than or equal to
+#and means if both are true the numbers are true

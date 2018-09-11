de278739d007710061dc82c0e96143f5ed02c9a4
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2018-09-11 08:45:51 -0700

    lab

---
+++ Unit2Alab/Unit2Alab.py
@@ -0,0 +1,35 @@
+t = 52
+y = 45.9
+
+num1 = input ()
+num2 = input ()
+
+print (num1 + str(num2))
+
+print (t + int(y))
+
+i = 100
+j = 200
+x = 300
+y = 100
+
+print ((i == j) or (i > x))
+
+print ((i < j) or (i >= y))
+
+print ((i != j) or (i != x))
+
+print ((i < j) or (i < x) or (i < y))
+
+#note
+x = -5
+#use "if" to
+if x > 0:
+    print ('x is positive')
+#use elif if you nneed more
+elif x < 0:
+    print ('x is negative')
+#if "if" is wrong what "else" is it
+else:
+    print ("x = 0")
+

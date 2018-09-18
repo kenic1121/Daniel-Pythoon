102ff295a7667e3c71a3db33f7d10eb30bc71a0b
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2018-09-18 08:43:16 -0700

    calculator

---
+++ Calculator/Calculator.py
@@ -0,0 +1,14 @@
+calcnum1 = input ('1st number to calculate:')
+calcnum2 = input ('2nd number to calculate')
+sign = input ('Would you like to multiply (*), Divide (/), subtract (-), or add (+):')
+
+
+
+
+
+
+print (calcnum1)
+print (calcnum2)
+print (sign)
+if sign == ("*")
+    print

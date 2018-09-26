050f236f61d33b15c9f3bb6ce30b76b0dedbe2f4
ubuntustudent <janielsupreme@gmail.com>
2018-09-26 10:05:26 -0700

    adjsa

--- Lab123/Lab123.py
+++ Lab123/Lab123.py
@@ -1,35 +1,43 @@
-from typing import List, Union
-
 
 def main ():
     grade = input('What grade are you in?: ')
+    letterGrade = gradeInSchool(grade)
+    print (letterGrade)
 
-    if grade == ('9')  :
-        print ('You are a Freshman.')
 
-    elif grade == ('10') :
+def list () :
+    intro = input('Type anything to find your average grade: ')
+    gradelist = [86.5 , 75.3 , 98.9 , 100]
+    p = gradelist[0]
+    p1 = gradelist[1]
+    p2 = gradelist[2]
+    p3 = gradelist[3]
+    length = len(gradelist)
+    add = (p + p1 + p2 + p3)
+    print (intro)
+    print (add / length)
+
+#def lettergrade () :
+#    if
+
+def gradeInSchool (mynumber) :
+    if mynumber == ('9')  :
+        return 'Freshman'
+
+
+    elif mynumber == ('10') :
         print ('You are a sophomore.')
 
-    elif grade == ('11')   :
+    elif mynumber == ('11')   :
         print ('You are a Junior.')
 
-    elif grade == ('12')   :
+    elif mynumber == ('12')   :
         print ('You are a senior.')
 
     else :
         print ('You arent in highschool')
 
-def list () :
-    gradelist = [86.5 , 75.3 , 98.9 , 100, 83.6]
-    pop = gradelist[1]
-    pop1 = gradelist[2]
-    pop3 = gradelist[3]
-    pop4 = gradelist[4]
-    pop5 = gradelist[0]
-    print(pop + pop1 + pop3 + pop4 + pop5)
+main()
 
 
-
-list()
-#main()
 

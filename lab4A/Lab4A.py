ee20a97ed2e07538398a0f4ae41408ea95eab435
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2018-10-23 08:56:01 -0700

    epic

--- lab4A/Lab4A.py
+++ lab4A/Lab4A.py
@@ -1,7 +1,22 @@
-def main :
-    string = input ('Insert your string here: ')
+def main () :
+    string = str (input('Place your word here: '))
+    password = deVowel(string)
+
 
     print (string)
+    
+    print (password)
+
+
+def deVowel(word):
+    List = list(word)
+    Vowels = ["a", "e", "i", "o", "u"]
+    for letter in List:
+        if letter.lower() in Vowels:
+            List.remove(letter)
+    word = ''.join(List)
+    return word
+
 
 
 main()

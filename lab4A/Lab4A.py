82ccf16ccc8a91ac14bbc872148eb844ee669959
ubuntustudent <janielsupreme@gmail.com>
2018-10-25 08:32:32 -0700

    epic

--- lab4A/Lab4A.py
+++ lab4A/Lab4A.py
@@ -2,7 +2,8 @@ def main () :
     string = str (input('Place your word here: '))
     password = deVowel(string)
     epic = [1 ,2 , 3 , 4]
-    pass2 = mathstuff(epic)
+    addnum = 4
+    pass2 = mathstuff(epic, addnum)
 
     print (string)
     print (password)
@@ -18,8 +19,13 @@ def deVowel(word):
     word = ''.join(List)
     return word
 
-def mathstuff(epicmath) :
-    maththing = epicmath.append('0')
-    print (maththing)
+def mathstuff(epicmath, multiply) :
+    list2 = []
+    for i in range( len(epicmath)):
+        maththing = epicmath.pop() * multiply
+        list2.append(maththing)
+    return list2
+
+
 
 main()

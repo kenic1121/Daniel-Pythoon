0ae663b0949810418e2dda0c5d22a8ab8e7caedd
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2018-10-25 09:17:54 -0700

    o m

---
+++ Lab4B/Lab4B.py
@@ -0,0 +1,13 @@
+
+for i in range (1,6) :
+    sum = i + 10
+    print (str(i) + "+10=" + str(sum))
+    product = i * 10
+    print (str(i) + "*10=" + str(product))
+
+list = [10,20,30,40,50]
+print (list)
+for o in range (len (list)) :
+    multiply = list[o] *10
+    list[o] = multiply
+print (list)

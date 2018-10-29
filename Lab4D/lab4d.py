37f0a6008579b48a518bbdddb5a82044f013f662
ubuntustudent <janielsupreme@gmail.com>
2018-10-29 09:20:18 -0700

    k

---
+++ Lab4D/lab4d.py
@@ -0,0 +1,7 @@
+shoppingcart = [['tooth paste' , 'q-tips' , 'milk'] , ['milk' , 'candy' , 'apples'] ,['paper' , 'pencils' , 'q-tips']]
+
+def allinone (shoppingcart) :
+    for list in shoppingcart:
+        for item in list :
+            
+

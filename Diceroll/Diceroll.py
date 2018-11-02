b5b9f1612efbca4e580a8cda8716c0e8463ac97d
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2018-11-02 09:15:32 -0700

    k

--- Diceroll/Diceroll.py
+++ Diceroll/Diceroll.py
@@ -2,21 +2,33 @@ import random
 
 def main () :
     setOfDice=[0] * 6
-    setOfDice = definedice()
+    #setOfDice = definedice()
+
     play = 'y'
     game = 0
+
+
     while play == 'y' :
         for i in range (0,22) :
             print ('_', end='')
-        print ()
+        print()
+        roll = rollDice()
+
+
+        dieroll = definedice(roll)
+        printMyDie(dieroll)
+      #  printDiceSideBySide(mydice)
+
         game += 1
         print (' game - ' + str(game))
         play = input('play again -y or -n ')
+
     print ('You played ' + str(game) + ' times.')
 
 
 
-def definedice () :
+
+def definedice (myRoll) :
     dice = [0] * 6
     topbot = ' ------- '
     blank =  '|       |'
@@ -30,7 +42,7 @@ def definedice () :
             dice[num]= [topbot,blank,oneDotM,blank,topbot]
         elif num == 1:
             dice[num] = [topbot,blank, twoDot, blank,topbot]
-        elif num == 2:
+        elif num == 2 :
             dice[num]= [topbot,oneDotL, oneDotM,oneDotR,topbot]
         elif num == 3:
             dice[num]= [topbot,twoDot,blank,twoDot,topbot]
@@ -38,5 +50,23 @@ def definedice () :
             dice[num]= [topbot,twoDot,oneDotR,twoDot,topbot]
         else:
             dice[num]= [topbot,twoDot,twoDot,twoDot,topbot]
-    return dice
+    return dice[myRoll]
 
+def rollDice() :
+    print ('roll dice')
+    diceNum = random.randint(0 , 5)
+    return diceNum
+
+def printMyDie (myDieRoll) :
+    for row in myDieRoll :
+        print(row)
+
+def printDiceSideBySide(diceSet) :
+    print('print dice')
+    for row in range (0 , 0):
+        for col in range (0 , 0):
+            print(diceSet[col][row] , end = '/t')
+        print()
+    return diceSet
+
+main()

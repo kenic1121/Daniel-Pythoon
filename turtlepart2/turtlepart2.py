11116b519e1e82b532eacf06d7212f61aa6e307f
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2018-11-28 10:06:28 -0800

    k

--- turtlepart2/turtlepart2.py
+++ turtlepart2/turtlepart2.py
@@ -3,14 +3,20 @@ import turtle
 t=turtle.Pen()
 
 
-def main () :
-    color = input('What is the color?: ')
+def main():
+    color = input('What is the color?:')
 
     turtle.pencolor(color)
 
-    sidelength = input('What is the side length?:  ')
+    sidelength = input('What is the side length? ')
 
-    turtle.forward(sidelength)
+    sides = input('How many sides do you want? : ')
+
+    for u in range (0, int(sides)) :
+        turtle.forward(int(sidelength))
+        turtle.left(float(360) / int(sides))
+
+
 
 
 main()

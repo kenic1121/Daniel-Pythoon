7fc95b39c605778bec42aa6dceadd37c11d340ea
ubuntustudent <janielsupreme@gmail.com>
2018-12-04 09:16:29 -0800

    k

--- Unit6ALab/Unit6ALab.py
+++ Unit6ALab/Unit6ALab.py
@@ -3,40 +3,45 @@ TextCodes = {'think' : 'have a particular opinion, belief, or idea about someone
 
 #prints the deffinition
 #print (TextCodes['think'])
+
 search = input('Would you like to search or change a word in the dictionary?    ')
 
 if search == 'search' :
-    wordfind = input('What word do you want the deffinition of?    ')
+        wordfind = input('What word do you want the deffinition of?    ')
+        wordfinding()
 
 elif search == 'change' :
-    input('What word would you like to change?  ')
-
-
-
-
-
-
-if wordfind == 'think' :
-    print(TextCodes['think'])
-
-elif wordfind == 'ear' :
-    print(TextCodes['ear'])
-
-elif wordfind == 'fly' :
-    print(TextCodes['fly'])
-
+        input('What word would you like to change?  ')
 else :
-    newword = input('Is this a new word?  ')
+    print('Exiting')
 
 
 
 
-if newword == 'yes' :
-   deffinition = input('What is the deffinition of your word?')
-   TextCodes[wordfind] = deffinition
-elif newword == 'no' :
-    input('click any key to exit')
-else:
-    print('error')
+def wordfinding () :
+
+
+    if wordfind == 'think' :
+        print(TextCodes['think'])
+
+    elif wordfind == 'ear' :
+        print(TextCodes['ear'])
+
+    elif wordfind == 'fly' :
+        print(TextCodes['fly'])
+
+    else :
+        newword = input('Is this a new word?  ')
+
+
+
+
+    if newword == 'yes' :
+        deffinition = input('What is the deffinition of your word?')
+        TextCodes[wordfind] = deffinition
+    elif newword == 'no' :
+        input('click any key to exit')
+    else:
+        print('error')
 
 

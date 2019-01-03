6471973022bcbb0f5ba5f570ed5b52a95e75aad6
kenic1121 <42872424+kenic1121@users.noreply.github.com>
2019-01-03 09:17:21 -0800

    as

---
+++ Lab7B/Lab7B.py
@@ -0,0 +1,13 @@
+class pet:
+    def __init__(self, name, breed):
+        self.pname = name
+        self.pbreed = breed
+
+class dog(pet):
+    def __init__(self, Pet):
+        self.Pet= Pet
+        activity = input('What activity does the dog do?')
+        print (activity)
+
+
+

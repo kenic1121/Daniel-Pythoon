8534ca53c8b0391ea5d43735753429e0a72452ad
ubuntustudent <janielsupreme@gmail.com>
2019-01-07 08:48:51 -0800

    as

--- Lab7B/Lab7B.py
+++ Lab7B/Lab7B.py
@@ -3,11 +3,24 @@ class pet:
         self.pname = name
         self.pbreed = breed
 
+    def info(self):
+        return(self.pname + " " + self.pbreed)
+
 class dog(pet):
-    def __init__(self, Pet):
-        self.Pet= Pet
-        activity = input('What activity does the dog do?')
-        print (activity)
+    def __init__(self, activity):
+        self.activity = activity
+
+    def info2(self):
+        return (self.activity)
+
+def Pets () :
+    print("_________Pet #1_________")
+    myPet1 = pet("Spot" , "dog")
+    print(myPet1.info())
+
+    print("_________Pet #2_________")
+    myPet2 = dog("Runs")
+    print(myPet1.info() , myPet2.info2 ())
 
 
-
+Pets()
